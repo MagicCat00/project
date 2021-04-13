c4a9df98a0107706fd337c7a0d3e841581a1e32f
MagicCat00 <75489180+MagicCat00@users.noreply.github.com>
2021-04-13 12:00:38 +0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,26 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+const {src, dest, watch} = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
 
-  const gulp = require('gulp');
-  const { gunzip } = require('node:zlib');
-  
-  const browserSync = require('browser-sync').create();
-  
-  gulp.task('hello', function(done) {
-      console.log('Привет, мир!');
-      done();
-  })
-  
-  //Static server
-  gulp.task('browser-sync', function() {
-      browserSync.init({
-          server: {
-              baseDir: "./"
-          }
-      });
-      gulp.watch("./*.html").on('change', browserSync.reload);
-  });
+
+// Static server
+function bs() {
+    serveSass();
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+watch("./*.html").on('change', browserSync.reload);
+watch("./sass/**/*.sass", serveSass);
+watch("./js/*.js").on('change', browserSync.reload);
+};
+
+function serveSass() {
+    return src("./sass/*.sass")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
